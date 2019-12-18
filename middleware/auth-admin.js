0460c17fa6a444521efed063b6a7720432bb3e6f
Kevin Jordan <macdev2@Kevins-MacBook-Pro.local>
2019-12-17 21:20:58 -0800

    good

--- middleware/auth-admin.js
+++ middleware/auth-admin.js
@@ -1,9 +1,13 @@
 export default function (context) {
-    // console.log('[running auth admin]')
+    console.log('[running auth admin]')
+    console.log(context.store.getters['reservation/getActiveHome'])
 
     if(context.store.getters['auth/getIsAdmin'] == false) {
         console.log('redirecting')
-        context.redirect('/')
+        
+            context.redirect('/')
+
+        
     } 
     return;
 }
